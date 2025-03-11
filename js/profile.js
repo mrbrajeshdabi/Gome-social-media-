e104f20dad933f9b53e4d5e04a19be88935a3cb7
unknown <testingforwebsite138@gmail.com>
2025-03-11 04:46:27 -0700

    add new function

--- js/profile.js
+++ js/profile.js
@@ -3,16 +3,16 @@ img = document.querySelector("#img"),
 usericon = document.querySelector("#usericon").addEventListener("click",profile);
 function profile()
 {
-  var file = document.createElement("INPUT");
+  let file = document.createElement("INPUT");
   file.setAttribute("type","file");
   file.click()
   file.onchange=function()
   {
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.readAsDataURL(this.files[0])
     reader.onload=function()
     {
-      var url = reader.result;
+      let url = reader.result;
       img.src=url;
       img.style.backgroundPosition="center";
       img.style.backgroundSize="cover";
